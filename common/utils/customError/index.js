1d9f4c5ef6d5e7be5c62950f8ceb218cc51d802e
Lucas P. Silva <lucas.pereira.silva@accenture.com>
2022-04-25 14:01:25 -0300

    chore: fix code indentation

--- common/utils/customError/index.js
+++ common/utils/customError/index.js
@@ -1,15 +1,14 @@
 class CustomError extends Error {
-    constructor({
-      statusCode = null,
-      message = null,
-    }) {
-      super(message);
-      this.statusCode = statusCode || 500;
-      this.message = message || 'Something bad happened!';
-    }
+  constructor({
+    statusCode = null,
+    message = null,
+  }) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.message = message || 'Something bad happened!';
   }
-  
-  module.exports = {
-    CustomError,
-  };
-  
+}
+
+module.exports = {
+  CustomError,
+};
